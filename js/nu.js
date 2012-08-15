413d2660cc402429b4ab8b6b318d036dcc5e6d5c
John Mettraux <jmettraux@gmail.com>
2012-08-15 15:20:48 +0900

    add nu

---
+++ js/nu.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2012-2012, John Mettraux, jmettraux@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Made in Japan.
+ */
+
+//
+// Nu, not underscore.
+//
+// Inspired by Jeremy Ashkenas' underscore.
+//
+var Nu = (function() {
+
+  //
+  // each
+
+  var breaker = {};
+
+  function rawEach(coll, func) {
+    for (var i in coll) {
+      var r = func(i, coll[i]);
+      if (r === breaker) break;
+    }
+    return coll;
+  }
+
+  this.each = function(coll, func) {
+    return rawEach(coll, function(k, v) {
+      if (coll instanceof Array) func(v, k); else func(k, v);
+    });
+  };
+
+  //
+  // detect, find
+
+  this.detect = function(coll, func) {
+    var ar = (coll instanceof Array);
+    var result = undefined;
+    rawEach(coll, function(k, v) {
+      var r = (ar) ? func(v, k) : func(k, v);
+      if (r) {
+        result = ar ? v : [ k, v ];
+        return breaker;
+      }
+    });
+    return result;
+  }
+  this.find = this.detect;
+
+  //
+  // collect, map
+
+  this.collect = function(coll, func) {
+    var ar = (coll instanceof Array);
+    var result = [];
+    rawEach(coll, function(k, v) {
+      result.push((ar) ? func(v, k) : func(k, v));
+    });
+    return result;
+  }
+  this.map = this.collect;
+
+  //
+  // over.
+
+  return this;
+
+}).apply({});
+
---
+++ test/qu_nu_test.js
@@ -0,0 +1,86 @@
+
+//
+// testing Nu
+//
+// Tue Aug 14 16:31:18 JST 2012
+//
+
+
+//
+// testing John.parse(s)
+
+function j_assert(source, expected, message) {
+
+  equal(
+    JSON.stringify(John.parse(source)),
+    JSON.stringify(expected),
+    message)
+}
+
+test('Nu.each(array)', function() {
+
+  var i = 0;
+
+  Nu.each([ 1, 2, 3 ], function(e) {
+    i = i + e;
+  });
+
+  equal(i, 6);
+});
+
+test('Nu.each(array) with index', function() {
+
+  var a = [];
+
+  Nu.each([ 1, 2, 3 ], function(e, i) {
+    a.push(e + ':' + i);
+  });
+
+  equal(a.join(', '), '1:0, 2:1, 3:2');
+});
+
+test('Nu.each(object)', function() {
+
+  var a = [];
+
+  Nu.each({ a: 1, b: 2, c: 3}, function(k, v) {
+    a.push(k + ':' + v);
+  });
+
+  equal(a.join(', '), 'a:1, b:2, c:3');
+});
+
+test('Nu.find(array)', function() {
+
+  var r = Nu.find([ 'alpha', 'bravo', 'charly' ], function(e, i) {
+    return e.match(/^b/)
+  });
+
+  equal(r, 'bravo');
+});
+
+test('Nu.find(object)', function() {
+
+  var r = Nu.find({ alpha: 10, bravo: 50, charly: 70 }, function(k, v) {
+    return v > 40;
+  });
+
+  equal(r.join(':'), 'bravo:50');
+});
+
+test('Nu.map(array)', function() {
+
+  var r = Nu.map([ 1, 2, 3 ], function(e) { return e * 2; });
+
+  equal(r.join(', '), '2, 4, 6');
+});
+
+test('Nu.map(object)', function() {
+
+  var r = Nu.map({ alpha: 10, bravo: 50, charly: 70 }, function(k, v) {
+    return v / 10;
+  });
+
+  equal(r.join(', '), '1, 5, 7');
+});
+
