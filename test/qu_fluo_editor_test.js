64134067896ee633a663fda10ffaea5e8982d83f
John Mettraux <jmettraux@gmail.com>
2013-04-21 14:21:38 +0900

    simplify tests

--- test/qu_fluo_editor_test.js
+++ test/qu_fluo_editor_test.js
@@ -5,36 +5,36 @@
 // Tue Aug 14 01:13:12 JST 2012
 //
 
-function jpequal(actual, expected, message) {
+function eq(actual, expected, message) {
 
   deepEqual(John.parse('{' + actual + '}'), expected, message)
 }
 
 test('attributes parse test', function() {
 
-  jpequal('alpha', { alpha: null });
-  jpequal('"alpha"', { alpha: null });
+  eq('alpha', { alpha: null });
+  eq('"alpha"', { alpha: null });
 
-  jpequal('ref:alpha', { ref: 'alpha' });
-  jpequal('ref:"alpha"', { ref: 'alpha' });
-  jpequal('ref: "alpha"', { ref: 'alpha' });
+  eq('ref:alpha', { ref: 'alpha' });
+  eq('ref:"alpha"', { ref: 'alpha' });
+  eq('ref: "alpha"', { ref: 'alpha' });
 
-  jpequal('"ref":"alpha"', { ref: 'alpha' });
-  jpequal('"ref": "alpha"', { ref: 'alpha' });
+  eq('"ref":"alpha"', { ref: 'alpha' });
+  eq('"ref": "alpha"', { ref: 'alpha' });
 
-  jpequal(' "${f:success}"', { '${f:success}': null });
+  eq(' "${f:success}"', { '${f:success}': null });
 
-  jpequal('alpha, "task": "clean lab"', { alpha: null, task: 'clean lab' });
-  jpequal('"alpha", "task": "clean lab"', { alpha: null, task: 'clean lab' });
+  eq('alpha, "task": "clean lab"', { alpha: null, task: 'clean lab' });
+  eq('"alpha", "task": "clean lab"', { alpha: null, task: 'clean lab' });
 
-  jpequal('"val":1, "var":[2]', { val: 1, 'var': [ 2 ] });
+  eq('"val":1, "var":[2]', { val: 1, 'var': [ 2 ] });
 
-  jpequal('"name": "my def", "revision": 0', { name: 'my def', revision: 0 });
+  eq('"name": "my def", "revision": 0', { name: 'my def', revision: 0 });
 
-  jpequal('${f:nada}', { '${f:nada}': null });
-  jpequal('"${f:nada}"', { '${f:nada}': null });
+  eq('${f:nada}', { '${f:nada}': null });
+  eq('"${f:nada}"', { '${f:nada}': null });
 
-  jpequal('${f:next}, "if": "${f:ok}"', { '${f:next}': null, 'if': '${f:ok}' });
-  jpequal('"${f:next}", "if": "${f:ok}"', { '${f:next}': null, 'if': '${f:ok}' });
+  eq('${f:next}, "if": "${f:ok}"', { '${f:next}': null, 'if': '${f:ok}' });
+  eq('"${f:next}", "if": "${f:ok}"', { '${f:next}': null, 'if': '${f:ok}' });
 });
 
